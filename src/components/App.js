0c5e529f667a1b2a2a0e66a7a7ca458b4ae4ee99
shekinah <nyamburamary1212@gmail.com>
2023-10-26 14:22:26 +0300

    complete state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  // Step 2: Create a state variable for dark mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // Step 4: Conditionally set the className based on isDarkMode
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  // Step 3: Create an event handler to toggle dark mode
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        {/* Step 3: Add an event handler to the button */}
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // Step 1: Create a state variable to track if the item is in the cart
+  const [inCart, setInCart] = useState(false);
+
+  // Step 2: Create an event handler to toggle the item in and out of the cart
+  const toggleCart = () => {
+    setInCart(!inCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Step 3: Conditionally set the button text based on inCart state */}
+      <button className="add" onClick={toggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
